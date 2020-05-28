59defadd6b8edb2aab89ba8f14aa9eea335ee01f
fireb1003 <34112797+fireb1003@users.noreply.github.com>
2020-05-28 03:03:08 +0200

    add material-ui

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useMyFetch } from "../utils/useMyFetch";
+import Button from "@material-ui/core/Button";
 
 export default function Home() {
   const { loading, data } = useMyFetch(
@@ -7,5 +8,12 @@ export default function Home() {
     {}
   );
   if (loading) return <div>Loading ...</div>;
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      {JSON.stringify(data)}
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
 }
